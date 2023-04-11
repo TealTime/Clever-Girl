581da6e4bd752fe6a08153d2eadb6aee9e2c1557
TealTime <>
2023-04-11 16:52:32 -0700

    Added Harmony Patch to confirm dismissing of companions
    Since hotkeys changed recently, Dismiss command changed to "d" instead
    of "D". I've hit dismiss by accident a couple times already, so I
    figured adding a safety check would be a good idea.

---
+++ src/HarmonyPatches/Dismiss_HandleEvent_Patch.cs
@@ -0,0 +1,39 @@
+namespace XRL.World.Parts.CleverGirl {
+    using HarmonyLib;
+    using System;
+    using XRL.UI;
+    using XRL.World.Effects;
+
+    public static class Helpers {
+        public static bool DoubleCheck(GameObject companion, GameObject Leader) {
+            if (Leader != The.Player) {
+                return true;
+            }
+            return Popup.ShowYesNo(string.Format("Do you really want to dismiss {0} from {1} service?",
+                                   companion.GetDisplayName(),
+                                   Leader.its)) == DialogResult.Yes;
+        }
+    }
+
+    // Simply prompt for yes or no confirmation prior to dismissing rebuked companions
+    [HarmonyPatch(typeof(Rebuked), "HandleEvent", new Type[] { typeof(InventoryActionEvent) })]
+    public static class Rebuked_HandleEvent_InventoryActionEvent_Patch {
+        public static bool Prefix(InventoryActionEvent E, Rebuked __instance) {
+            if (E.Command == "DismissServitor" && E.Actor == __instance.Rebuker && E.Item == __instance.Object && __instance.Rebuker.CheckCompanionDirection(__instance.Object)) {
+                return Helpers.DoubleCheck(__instance.Object, __instance.Rebuker);
+            }
+            return true;
+        }
+    }
+
+    // Simply prompt for yes or no confirmation prior to dismissing proselytized companions
+    [HarmonyPatch(typeof(Proselytized), "HandleEvent", new Type[] { typeof(InventoryActionEvent) })]
+    public static class Proselytized_HandleEvent_InventoryActionEvent_Patch {
+        public static bool Prefix(InventoryActionEvent E, Proselytized __instance) {
+            if (E.Command == "DismissProselyte" && E.Actor == __instance.Proselytizer && E.Item == __instance.Object && __instance.Proselytizer.CheckCompanionDirection(__instance.Object)) {
+                return Helpers.DoubleCheck(__instance.Object, __instance.Proselytizer);
+            }
+            return true;
+        }
+    }
+}
