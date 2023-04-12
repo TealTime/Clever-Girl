3354067d0cd0f6e6f607aa09f1f55a4ff7f8d287
TealTime <>
2023-04-11 17:38:28 -0700

    Small edit to dialog

--- src/Menus/MainMenu.cs
+++ src/Menus/MainMenu.cs
@@ -8,29 +8,9 @@ namespace XRL.World.CleverGirl {
 
 
     public class CleverGirl_MainMenu {
-
-        // TODO: Make these remarks more immersive by adding conditions and priority system. Also account for
-        //       lack of mouths and/or telepathy.
-        //       (IE: "Remember that [dmg_source] you hit me with [number] turns ago? Cause I sure do.")
-        public static readonly List<Func<GameObject, GameObject, string>> REMARKS = new List<Func<GameObject, GameObject, string>> {
-            // (leader, companion)
-            (l, c) => "How's it hanging, leader?",
-            (l, c) => "Where'd my sunglasses go?",
-            (l, c) => "What's up boss?",
-            (l, c) => "I swear I saw another " + c.GetApparentSpecies() + " earlier that looked just like me!",
-            (l, c) => "Are we stopping any time soon?",
-            (l, c) => "When was the last time you slept!?",
-            (l, c) => "What now?",
-            (l, c) => "Live and drink!",
-            (l, c) => "Your thirst is yours, my water is MINE.",
-        };
-        private static readonly Random rng = new Random();
-        private static List<int> rngSequence = null;
-        private static int rngIndex = 0;
-
         public static readonly Utility.InventoryAction ACTION = new Utility.InventoryAction {
             Name = "Clever Girl - Main Menu",
-            Display = "\"{{inventoryhotkey|c}}lever girl\"",
+            Display = "issue a {{inventoryhotkey|c}}lever command",
             Command = "CleverGirl_MainMenu",
             Key = 'c',
             Valid = Utility.InventoryAction.Adjacent,
@@ -45,6 +25,24 @@ namespace XRL.World.CleverGirl {
             CleverGirl_BehaviorsMenu.ACTION,
         };
 
+        // TODO: Make these remarks more immersive by adding conditions and priority system. Also account for
+        //       lack of mouths and/or telepathy.
+        //       (IE: "Remember that [dmg_source] you hit me with [number] turns ago? Cause I sure do.")
+        public static readonly List<Func<GameObject, GameObject, string>> REMARKS = new List<Func<GameObject, GameObject, string>> {
+            // (leader, companion)
+            (l, c) => "How's it hanging, friend?",
+            (l, c) => "Where'd my shades go?",
+            (l, c) => "What's up boss?",
+            (l, c) => "Are we stopping any time soon?",
+            (l, c) => "When was the last time you slept!?",
+            (l, c) => "What now?",
+            (l, c) => "Live and drink!",
+            (l, c) => "Your thirst is yours, my water is MINE.",
+        };
+        private static readonly Random rng = new Random();
+        private static List<int> rngSequence = null;
+        private static int rngIndex = 0;
+
         public static bool Start(GameObject leader, GameObject companion) {
             // Shuffle remarks instead of selecting randomly each time. Because pseudo-random is only pseudo-fun.
             if (rngSequence == null || rngIndex >= rngSequence.Count) {
@@ -56,14 +54,13 @@ namespace XRL.World.CleverGirl {
             string remark = Options.ShowSillyText ? REMARKS[rngSequence[rngIndex]](leader, companion) : "";
             rngIndex++;
 
-            bool result = false;
-            result = CleverGirl_BasicMenu.Start(leader, companion, OPTIONS,
-                                                Title: companion.ShortDisplayName,
-                                                Intro: remark,
-                                                centerIntro: true,
-                                                IntroIcon: companion.RenderForUI(),
-                                                AllowEscape: true);
-            return result;
+
+            return CleverGirl_BasicMenu.Start(leader, companion, OPTIONS,
+                                              Title: companion.ShortDisplayName,
+                                              Intro: remark,
+                                              centerIntro: true,
+                                              IntroIcon: companion.RenderForUI(),
+                                              AllowEscape: true);
         }
     }
 }
